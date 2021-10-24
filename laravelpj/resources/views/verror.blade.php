09e1b0bb01ed942d5ced8d3ffc762731ad5c7a92
村上　慧太 <k33sat@gmail.com>
2021-10-24 12:32:42 +0900

    first-commit

--- laravelpj/resources/views/verror.blade.php
+++ laravelpj/resources/views/verror.blade.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>COACHTECH</title>
 </head>
-
 <body>
-  <form action="test.php" method="post">
-    <input type="text" name="name">
-    <input type="submit" value="送信">
-  </form>
+  <p>入力内容にエラーがありました。</p>
 </body>
-
 </html>
--- test.php
+++ test.php
@@ -1,3 +1,18 @@
-<?php
-$Individual =htmlspecialchars($_POST['Individual'],ENT_QUOTES);
-print $Individual ."さんご登録ありがとうございます";
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  $name = $_POST['name'];
+  print($name . "さんご登録ありがとうございます");
+  ?>
+</body>
+
+</html>

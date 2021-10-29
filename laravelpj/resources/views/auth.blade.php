2e4aadf1cfd0dbf20925e9f64e6e450c28f3036a
村上　慧太 <k33sat@gmail.com>
2021-10-29 19:25:39 +0900

    再々々々々々々々修正

--- laravelpj/resources/views/auth.blade.php
+++ laravelpj/resources/views/auth.blade.php
@@ -21,9 +21,9 @@
 <form action="/auth" method="post">
   <table>
     @csrf
-    <tr><th>メール: </th><td><input type="text"
+    <tr><th>メール: </th><td><input type="text" 
           name="email"></td></tr>
-    <tr><th>パスワード: </th><td><input type="password"
+    <tr><th>パスワード: </th><td><input type="password" 
           name="password"></td></tr>
     <tr><th></th><td><input type="submit" 
           value="送信"></td></tr>

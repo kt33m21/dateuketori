3a79de629b1715a2df7aa301dbed172c719b4b17
村上　慧太 <k33sat@gmail.com>
2021-10-29 16:32:04 +0900

    再々々々々々修正

--- laravelpj/app/Http/Controllers/AuthorController.php
+++ laravelpj/app/Http/Controllers/AuthorController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Author;
 
@@ -9,7 +11,28 @@ class AuthorController extends Controller
 {
     public function index()
     {
-        $items = Author::Paginate(4);
-        return view('index', ['items' => $items]);
+        $user = Auth::user();
+        $items = Author::paginate(4);
+        $param = ['items' => $items, 'user' =>$user];
+        return view('index', $param);
+    }
+
+public function check(Request $request)
+    {
+    $text = ['text' => 'ログインして下さい。'];
+    return view('auth', $text);
+    }
+
+    public function checkUser(Request $request)
+    {
+    $email = $request->email;
+    $password = $request->password;
+    if (Auth::attempt(['email' => $email,
+            'password' => $password])) {
+        $text =   Auth::user()->name . 'さんがログインしました';
+    } else {
+        $text = 'ログインに失敗しました';
+    }
+    return view('auth', ['text' => $text]);
     }
 }
--- laravelpj/routes/web.php
+++ laravelpj/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controlllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home',[AuthorController::class, 'index']);
+Route::get('/add',[AuthorController::class, 'add']);
+Route::post('/add',[AuthorController::class, 'create']);
+Route::get('/edit',[AuthorController::class, 'edit']);
+Route::post('/edit',[AuthorController::class, 'update']);
+Route::get('/delete',[AuthorController::class, 'delete']);
+Route::post('/delete',[AuthorController::class, 'remove']);
+Route::get('/find',[AuthorController::class, 'find']);
+Route::post('/find',[AuthorController::class, 'search']);
+Route::get('/author/{author}',[AuthorController::class, 'bind']);
+Route::get('/auth', [AuthorController::class,'check']);
+Route::post('/auth' , [AuthorController::class, 'checkUser']);
+Route::prefix('book')->group(function(){
+  Route::get('/',[Bookcontroller::class, 'index']);
+  Route::get('/add',[BookController::class, 'add']);
+  Route::post('/add',[BookController::class, 'create']);
+});
+Route::get('/relation',[AuthorController::class, 'relate']);
+
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
+
+Route::get('/', function(){
+  return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard',function(){
+  return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
